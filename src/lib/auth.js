ccc6ef2c6ba3d7b0ce047d0481566daa12c54c01
SIM Ruhan <simruhan22@gmail.com>
2026-04-30 18:41:12 +0600

    aall

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,14 +1,11 @@
 import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { MongoClient } from "mongodb";
-import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("pic-gen-app");
+const db = client.db();
+
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
-  }),
-  emailAndPassword:{
-    enabled: true
-  }
+    database: mongodbAdapter(db),
+    // ... your other config (email/password, social, etc.)
 });
